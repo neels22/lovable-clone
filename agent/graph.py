1172235eb272af4eede0d9ba7f1e2975a184e8dd
neels22 <indraneelsarode@gmail.com>
2025-09-02 22:52:28 -0700

    feat: added architect node architect class schema architect prompt

--- agent/graph.py
+++ agent/graph.py
@@ -2,8 +2,8 @@
 import os
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-from prompts import planner_prompt
-from states import Plan
+from prompts import planner_prompt,architect_prompt
+from states import Plan,TaskPlan
 from langgraph.graph import StateGraph, START, END
 load_dotenv()
 
@@ -23,8 +23,20 @@ def planner_agent(state: dict) -> dict:
     resp = llm.with_structured_output(Plan).invoke(planner_prompt(user_prompt))
     return {"plan": resp}
 
+
+def architect_agent(state: dict) -> dict:
+    plan = state["plan"]
+    resp = llm.with_structured_output(TaskPlan).invoke(architect_prompt(plan))
+    if resp is None:
+        raise ValueError("No task plan found")
+    resp.plan = plan # here the plan is added to keep the context going on forward possible because configdict allowed extra
+    return {"task_plan": resp}
+
+
 graph = StateGraph(dict)
 graph.add_node("planner",planner_agent)
+graph.add_node("architect",architect_agent) 
+graph.add_edge("planner","architect")
 graph.set_entry_point("planner")
 
 
@@ -32,3 +44,5 @@ agent = graph.compile()
 user_prompt = "create a simple calculator web application"
 result = agent.invoke({"user_prompt": user_prompt})
 print(result)
+
+
--- agent/states.py
+++ agent/states.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel,Field
+from pydantic import BaseModel,Field,ConfigDict
     
 
 
@@ -14,3 +14,12 @@ class Plan(BaseModel):
     techstack: str = Field(description="The tech stack to be used for the app, e.g. 'python', 'javascript', 'react', 'flask', etc.")
     features: list[str] = Field(description="A list of features that the app should have, e.g. 'user authentication', 'data visualization', etc.")
     files: list[File] = Field(description="A list of files to be created, each with a 'path' and 'purpose'")
+
+
+class ImplementationTask(BaseModel):
+    filepath: str = Field(description="The path to the file to be modified")
+    task_description: str = Field(description="A detailed description of the task to be performed on the file, e.g. 'add user authentication', 'implement data processing logic', etc.")
+
+class TaskPlan(BaseModel):
+    implementation_steps: list[ImplementationTask] = Field(description="A list of steps to be taken to implement the task")
+    model_config = ConfigDict(extra="allow")
